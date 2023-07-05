ed6b398eb41b6af2aa67311b9067ec4cd8cf62ba
Juno & Ice <70862998+JunoAndIce@users.noreply.github.com>
2023-07-05 13:54:04 -0500

    More HTML input / Psuedocode for javascript added

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+// The page should show a start game button.
+// The page should also show a win/loss counter
+// blank word bank appears on screen and timer starts
+// Text bank reads any singular key press on the keyboard and records it
+// Key pressed is displayed on screen in the correct spot on the word
+// When the user guesses the word correctly, the timer stops and records a win. Win count goes up by 1.
+// If timer runs out loss is recorded, and the user is given +1 loss. 
